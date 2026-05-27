a94b01fbe6136d51fdcd85cfd706bc201601007e
ejwestdev <edwardwestdev@gmail.com>
2026-05-27 18:15:50 -0400

    initial commit

---
+++ src/components/hero.rs
@@ -0,0 +1,10 @@
+use dioxus::prelude::*;
+
+const HEADER_SVG: Asset = asset!("/assets/header.svg");
+
+#[component]
+pub fn Hero() -> Element {
+    rsx! {
+        div {}
+    }
+}
---
+++ src/views/home.rs
@@ -0,0 +1,12 @@
+use crate::components::Hero;
+use crate::components::Body;
+use dioxus::prelude::*;
+
+/// The Home page component that will be rendered when the current route is `[Route::Home]`
+#[component]
+pub fn Home() -> Element {
+    rsx! {
+        Hero {}
+        Body {}
+    }
+}
